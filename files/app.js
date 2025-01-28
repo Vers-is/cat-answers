37667b39d324ada56150134f3526be7c76aa8f22
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2025-01-29 00:21:19 +0600

    main structue added

---
+++ files/app.js
@@ -0,0 +1,10 @@
+//////////////// COLOR
+const RED = "#FC1A44";
+const ORANGE = "#FFA51F";
+const BLUE = "#A6BFFF";
+const GREEN = "#7DCEBF";
+const PINK = "#EFB1CD";
+const PURPLE = "#D3B5E6";
+////////////////////////
+
+
