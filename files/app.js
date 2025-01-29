fbf4999862e08bf2e5c67e4e9fb157f218794662
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2025-01-29 13:58:43 +0600

    color logic added (not full)

--- files/app.js
+++ files/app.js
@@ -7,4 +7,21 @@ const PINK = "#EFB1CD";
 const PURPLE = "#D3B5E6";
 ////////////////////////
 
+document.querySelectorAll('.color-element').forEach(circle => {
+    const color = circle.getAttribute('data-color');
+    circle.style.backgroundColor = color;
+
+    circle.addEventListener('click', function () {
+        const button = document.getElementById('targetButton');
+        const input = document.getElementById('targetInput');
+        const cat = document.getElementById('targetCat');
+
+        document.documentElement.style.setProperty('--current-color', color);  
+
+        button.style.backgroundColor = color;
+        button.style.borderColor = color;
+        input.style.color = color;
+        cat.style.backgroundColor = color;
+    });
+});
 
